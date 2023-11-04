cf4a154fa1a1457a467cf0e789dc0af21c37db4c
Ivaylo Tsonev <ivaylo.tsonev@outlook.com>
2023-11-04 17:47:55 +0200

    Initial Commit

---
+++ Zenix-iOS/Zenix_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zenix_iOSApp.swift
+//  Zenix-iOS
+//
+//  Created by Ivaylo Tsonev on 04.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Zenix_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
